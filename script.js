1c5b7a9d1468e4d00e9cd727db0a3119e06fd725
niajeanpierre <nia.jeanpierre@mykenzie.snhu.edu>
2023-09-11 10:10:17 -0400

    completed set of 5

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Your Code Here.
 const dictionary = words;
-console.log(dictionary); // Check to see if all words are showing up from the word array in the word.js file.
 
 function areAnagrams(word1, word2) {
   return (
@@ -13,12 +12,12 @@ function findAnagrams(userWord, wordList) {
   return wordList.filter((word) => areAnagrams(userWord, word));
 }
 
-function displayResults(results) {
-  const resultsDiv = document.getElementById("results");
-  resultsDiv.innerHTML = "";
+function displayResults(results, containerId) {
+  const container = document.getElementById(containerId);
+  container.innerHTML = "";
 
   if (results.length === 0) {
-    resultsDiv.textContent = "No anagrams found for the given word.";
+    container.textContent = "No anagrams found for the given word.";
   } else {
     const ul = document.createElement("ul");
     results.forEach((anagram) => {
@@ -26,17 +25,56 @@ function displayResults(results) {
       li.textContent = anagram;
       ul.appendChild(li);
     });
-    resultsDiv.appendChild(ul);
+    container.appendChild(ul);
   }
 }
 
-function findAndDisplayAnagrams() {
+function findAnagramSets(wordList, setSize) {
+  const anagramSets = {};
+  
+  // Group words by their sorted letter patterns
+  for (const word of wordList) {
+    const sortedWord = word.toLowerCase().split("").sort().join("");
+    
+    if (!anagramSets[sortedWord]) {
+      anagramSets[sortedWord] = [word];
+    } else {
+      anagramSets[sortedWord].push(word);
+    }
+  }
+
+  // Filter anagram sets with at least 'setSize' words
+  return Object.values(anagramSets).filter(set => set.length >= setSize);
+}
+
+function displayAnagramSets(sets, containerId) {
+  const container = document.getElementById(containerId);
+  container.innerHTML = "";
+
+  if (sets.length === 0) {
+    container.textContent = `No sets of ${setSize} or more anagrams found.`;
+  } else {
+    sets.forEach(set => {
+      const ul = document.createElement("ul");
+      set.forEach(word => {
+        const li = document.createElement("li");
+        li.textContent = word;
+        ul.appendChild(li);
+      });
+      container.appendChild(ul);
+    });
+  }
+}
+
+// Find and display sets of 5 or more anagrams on page load
+const setSize = 5; // Define the set size (5 or more)
+const setsOfAnagrams = findAnagramSets(dictionary, setSize);
+displayAnagramSets(setsOfAnagrams, "anagramSets");
+
+document.getElementById("find").addEventListener("click", () => {
   const userWord = document.getElementById("inputText").value.trim();
   const anagramsList = findAnagrams(userWord, dictionary);
 
-  displayResults(anagramsList);
-}
-
-document.getElementById("find").addEventListener("click", () => {
-  findAndDisplayAnagrams();
+  displayResults(anagramsList, "results");
 });
+
